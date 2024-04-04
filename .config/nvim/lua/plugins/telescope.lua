929edbecdb5efdac33041093e01f8ab612cb00a5
Heidar Bernhardsson <heidar@heidarb.com>
2024-04-04 17:56:25 +0100

    do not use telescope smart path display

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -17,7 +17,6 @@ return {
 			defaults = {
 				prompt_prefix = " ",
 				selection_caret = " ",
-				path_display = { "smart" },
 				mappings = {
 					i = {
 						["<CR>"] = actions.select_default,
