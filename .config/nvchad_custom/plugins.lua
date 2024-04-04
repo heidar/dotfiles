f4d333de51fd941b69e52ccf4a33494c76443a23
Heidar Bernhardsson <heidar@heidarb.com>
2024-04-04 17:56:11 +0100

    add copilot

--- .config/nvchad_custom/plugins.lua
+++ .config/nvchad_custom/plugins.lua
@@ -1,91 +1,97 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
-  -- override plugin definition options
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	-- override plugin definition options
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "node_modules", "vendor" },
-      },
-    },
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = {
+			defaults = {
+				file_ignore_patterns = { "node_modules", "vendor" },
+			},
+		},
+	},
 
-  -- highlight same word
-  {
-    "RRethy/vim-illuminate",
-    lazy = true,
-    enabled = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    event = { "CursorMoved", "InsertLeave" },
-    config = function()
-      require("illuminate").configure {
-        filetypes_denylist = {
-          "neotree",
-          "neo-tree",
-          "Telescope",
-          "telescope",
-        },
-      }
-    end,
-  },
+	-- highlight same word
+	{
+		"RRethy/vim-illuminate",
+		lazy = true,
+		enabled = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = { "CursorMoved", "InsertLeave" },
+		config = function()
+			require("illuminate").configure({
+				filetypes_denylist = {
+					"neotree",
+					"neo-tree",
+					"Telescope",
+					"telescope",
+				},
+			})
+		end,
+	},
 
-  -- render markdown
-  {
-    "ellisonleao/glow.nvim",
-    config = true,
-    cmd = "Glow",
-  },
+	-- render markdown
+	{
+		"ellisonleao/glow.nvim",
+		config = true,
+		cmd = "Glow",
+	},
 
-  -- ledger support
-  {
-    "ledger/vim-ledger",
-    event = "BufEnter *.ldg",
-  },
+	-- ledger support
+	{
+		"ledger/vim-ledger",
+		event = "BufEnter *.ldg",
+	},
+
+	{
+		"github/copilot.vim",
+		cmd = "Copilot",
+		event = "InsertEnter",
+	},
 }
 
 return plugins
