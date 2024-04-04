6e9372689b3a80d1a1a1773df345985ba3c230f2
Heidar Bernhardsson <heidar@heidarb.com>
2024-04-04 17:56:16 +0100

    add bufferline.nvim

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+-- bufferline to show open buffers
+
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				style_preset = {
+					bufferline.style_preset.no_italic,
+				},
+				indicator = {
+					style = "none",
+				},
+				modified_icon = "●",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+			highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		})
+	end,
+}
