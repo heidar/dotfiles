e8625d95a9ba0e0e9c1210ccd3f134288bd9211d
Heidar Bernhardsson <heidar@heidarb.com>
2024-04-04 17:56:27 +0100

    add neotest to neovim

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,41 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-plenary",
+		"nvim-neotest/neotest-go",
+	},
+	config = function()
+		local neotest = require("neotest")
+		neotest.setup({
+			adapters = {
+				require("neotest-plenary"),
+				require("neotest-go"),
+			},
+		})
+
+		-- keybindings
+		local wk = require("which-key")
+		wk.register({
+			["<Leader>tr"] = {
+				function()
+					neotest.run.run()
+				end,
+				"Run nearest test",
+			},
+			["<Leader>tf"] = {
+				function()
+					neotest.run.run(vim.fn.expand("%"))
+				end,
+				"Run test file",
+			},
+			["<Leader>ts"] = {
+				function()
+					neotest.run.stop()
+				end,
+				"Stop nearest test",
+			},
+		})
+	end,
+}
